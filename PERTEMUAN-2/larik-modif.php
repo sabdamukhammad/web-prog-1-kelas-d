5023eb53ad959a7b33c7a78c68fccf8183a79607
sabdamukhammad <sabdamukhammad2014@gmail.com>
2018-02-27 16:17:40 +0700

    latihan pertemuan ke-2

---
+++ PERTEMUAN-2/larik-modif.php
@@ -0,0 +1,12 @@
+<?php
+    $data=array(
+        "nama"=> "Mukhammad Sabda",
+        "usia"=> 18,
+        "jomblo"=> true 
+    );
+
+    echo"Nama:  ".$data["nama"]."<br>";
+    echo"Usia:  ".$data["usia"]."<br>";
+    echo"Jomblo:    :".$data["jomblo"]."<br>";
+
+?>
