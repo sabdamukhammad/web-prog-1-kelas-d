07afe86f1ec840020a532ae466e6dcbeaf45611a
sabdamukhammad <sabdamukhammad2014@gmail.com>
2018-02-27 16:36:22 +0700

    latihan pertemuan ke-2

---
+++ PERTEMUAN-2/larik-multi.php
@@ -0,0 +1,24 @@
+<?php
+
+$data=array(
+    "web-prog"=>
+    array(
+        "Intan"=>5,
+        "Sabda"=>7,
+        "Ali"=>9,)
+    "kalkulus"=>
+    array(
+        "Ali"=>6,
+        "Sabda"=>9,
+        "Intan"=>8,)
+
+    );
+
+    echo"Nilai web-prog Intan:".
+    $data["web-prog"]["Intan"]."<br>";
+    echo"Nilai Kalkulus Sabda:".
+    $data["kalkulus"]["Sabda"]."<br>";
+    echo"Nilai web-prog Ali:".
+    $data["web-prog"]["Ali"]."<br>";
+
+?>
