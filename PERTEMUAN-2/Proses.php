e573b0bd58093b02f1a3671fffcebc69811346f9
sabdamukhammad <sabdamukhammad2014@gmail.com>
2018-02-27 16:54:27 +0700

    Pertemuan ke-2

---
+++ PERTEMUAN-2/Proses.php
@@ -0,0 +1,6 @@
+<h2>SELAMAT DATANG, <?php echo $_POST["Nama"]; ?>
+</h2>
+<br>
+<br>
+Email anda [<?php echo $_POST["Email"]; ?>]
+telah terekam
---
+++ PERTEMUAN-2/input.php
@@ -0,0 +1,6 @@
+<form action="Proses.php"method="post">
+    Nama Anda<input type ="text"name="nama"<br>
+    Email <input type="text"name="email"<br>
+    <input type="submit" value="Proses">
+
+    </form>
